3c92ee3bc439ad3331f49bd83e29e03e7a85efd2
bwhite03 <bwhite1195@yahoo.com>
2019-09-12 13:23:05 -0400

    fixed package issue

--- server.js
+++ server.js
@@ -8,14 +8,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "client/build")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 // Create Connection
 const db = mysql.createConnection({
   host: "us-cdbr-iron-east-02.cleardb.net",

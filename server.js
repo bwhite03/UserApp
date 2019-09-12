f247f34c4d2f58e1b97a83a00510de061601d41a
bwhite03 <bwhite1195@yahoo.com>
2019-09-12 15:09:58 -0400

    final fix for deployment

--- server.js
+++ server.js
@@ -8,13 +8,6 @@ const path = require("path");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("client/build"));
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
 
 // Create Connection
 const db = mysql.createConnection({
